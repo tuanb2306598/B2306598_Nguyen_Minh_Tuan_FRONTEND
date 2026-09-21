fa182529b8b3227f9876753d011b0952279d9c28
tuanb2306598 <minhtuan131225@gmail.com>
2026-09-21 11:42:18 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ const routes = [
         path: "/",
         name: "contactbook",
         component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
     }
 ];
 
